67fe6ee3c425ac55aff3d99e78edbb8a4dce6fac
figa12 <figa.figa@gmail.com>
2013-11-08 22:54:08 +0100

    lidt mere. noget test.

--- AutoAlarm/src/main/java/dk/figz/autoalarm/MainActivity.java
+++ AutoAlarm/src/main/java/dk/figz/autoalarm/MainActivity.java
@@ -1,103 +1,94 @@
 package dk.figz.autoalarm;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.content.ContentResolver;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
-import android.view.LayoutInflater;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.text.Format;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Cursor cursor = null;
+
+    private static final String[] COLS = new String[]
+            {
+                    CalendarContract.Events.TITLE,
+                    CalendarContract.Events.DTSTART
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Cursor hest = readCalendar();
-        processCalendarResults(hest);
+        cursor = getContentResolver().query(CalendarContract.Events.CONTENT_URI, COLS, null, null, null);
 
+        cursor.moveToFirst();
 
+        Button b = (Button) findViewById(R.id.next);
+        b.setOnClickListener(this);
+        b = (Button) findViewById(R.id.previous);
+        b.setOnClickListener(this);
+        onClick(findViewById(R.id.previous));
     }
 
 
-     @Override
-     public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-     // Inflate the menu; this adds items to the action bar if it is present.
-     getMenuInflater().inflate(R.menu.main, menu);
-     return true;
-     }
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-     @Override
-     public boolean onOptionsItemSelected(MenuItem item) {
-     // Handle action bar item clicks here. The action bar will
-     // automatically handle clicks on the Home/Up button, so long
-     // as you specify a parent activity in AndroidManifest.xml.
-     switch (item.getItemId()) {
-     case R.id.action_settings:
-     return true;
-     }
-     return super.onOptionsItemSelected(item);
-     }
-
-    // Projection array. Creating indices for this array instead of doing
-    // dynamic lookups improves performance.
-    public static final String[] EVENT_PROJECTION = new String[] {
-            CalendarContract.Calendars._ID,                           // 0
-            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
-            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
-            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
-    };
-
-    // The indices for the projection array above.
-    private static final int PROJECTION_ID_INDEX = 0;
-    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
-
-    public Cursor readCalendar() {
-
-        // Run query
-        Cursor cur = null;
-        ContentResolver cr = getContentResolver();
-        Uri uri = CalendarContract.Calendars.CONTENT_URI;
-        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
-                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?))";
-        String[] selectionArgs = new String[] {"figa.figa@gmail.com", "com.google"};
-        // Submit the query and get a Cursor object back.
-        return cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
-     }
-
-    public void processCalendarResults(Cursor cur) {
-        // Use the cursor to step through the returned records
-
-        TextView view = (TextView) findViewById(R.id.debug_text);
-        while (cur.moveToNext()) {
-            long calID = 0;
-            String displayName = null;
-            String accountName = null;
-            String ownerName = null;
-
-            // Get the field values
-            calID = cur.getLong(PROJECTION_ID_INDEX);
-            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
-            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
-            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
-
-            // Do something with the values...
-
-            view.append(displayName);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        TextView textView = (TextView) findViewById(R.id.data);
+
+        String title = "N/A";
+
+        Long start = 0L;
+
+        switch (v.getId()) {
+            case R.id.next:
+                if (!cursor.isLast()) cursor.moveToNext();
+                break;
+            case R.id.previous:
+                if (!cursor.isFirst()) cursor.moveToPrevious();
+                break;
+
+        }
+
+        Format df = DateFormat.getDateFormat(this);
+        Format tf = DateFormat.getTimeFormat(this);
+
+        try {
+            title = cursor.getString(0);
+
+            start = cursor.getLong(1);
+
+        } catch (Exception e) {
+            //ignore
+        }
+        textView.setText(title + " on " + df.format(start) + " at " + tf.format(start));
+    }
 }
